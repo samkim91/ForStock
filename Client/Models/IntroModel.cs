3f25b77886311b4d1fff2526b738a86ad8eb29f9
samkim91 <kimh616@gmail.com>
2021-03-02 22:22:40 +0900

    Done: Validation / Todo: makeFourthQuarterData error

--- Client/Models/IntroModel.cs
+++ Client/Models/IntroModel.cs
@@ -5,9 +5,7 @@ namespace ForStock.Client.Models
     public class IntroModel
     {
         [Key]
-        [Required]
         public string crtfc_key { get; set; }
-        [Required]
         public string corp_name { get; set; }
         public string stock_code { get; set; }
         public string fs_div { get; set; } = "OFS";
--- Shared/Common/CorpCodeHelper.cs
+++ Shared/Common/CorpCodeHelper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Xml.Linq;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,19 +7,20 @@ namespace ForStock.Server.Common
 {
     public static class CorpCodeHelper{
 
-        public static string GetCorpCode(string stockCode = null, string corpName = null)
+        public static string GetCorpCode(string key_word)
         {
             XElement root = XElement.Load("./Common/CORPCODE.xml");
 
             IEnumerable<XElement> result =
                 from el in root.Elements("list")
-                where (string) el.Element("stock_code") == stockCode || (string) el.Element("corp_name") == corpName
+                where (string) el.Element("stock_code") == key_word || (string) el.Element("corp_name") == key_word
                 select el;
 
             if(result.Count() > 0)
             {
                 return (string) result.First().Element("corp_code");
             }
+            
             return null;
         }
     }

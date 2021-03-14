9d199e484347c4c99e6679d23c1e919b69ad646d
samkim91 <kimh616@gmail.com>
2021-03-14 22:52:22 +0900

    Done: deployed / Todo: test and make it better

--- Server/Program.cs
+++ Server/Program.cs
@@ -21,6 +21,7 @@ namespace ForStock.Server
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:5000");
                 });
     }
 }
